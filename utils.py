37d9d3d47da22f2b254bc6901ad06815707aefad
Abril Diaco <adiaco@udesa.edu.ar>
2026-09-17 20:06:55 -0300

    coment

--- utils.py
+++ utils.py
@@ -349,7 +349,7 @@ def warpear(img, H, size_canvas):
 
     # Distacia al pixel negro (borde) mas cercano
     peso = cv2.distanceTransform(mascara, cv2.DIST_L2, 5)
-    peso = peso ** 4 # Multiplicamos por 4 para que los bordes tengan menos peso y no se vean tan marcados --> la imahgen final menos borrosa
+    peso = peso ** 4 # Elevamos a la 4 para que los bordes tengan menos peso y no se vean tan marcados --> la imagen final menos borrosa
 
     return img_warpeada, peso
 
